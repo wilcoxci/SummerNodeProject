29eda9e7d0fc78a20250a0b928bf9b7609d8fff0
wilcoxci <cody.henrichsen@740s-pl-998.local>
2016-07-26 14:31:23 -0600

    Model Folder

---
+++ SummerNodeProject/Model/DataNode.cpp
@@ -0,0 +1,9 @@
+//
+//  DataNode.cpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "DataNode.hpp"
---
+++ SummerNodeProject/Model/DataNode.hpp
@@ -0,0 +1,14 @@
+//
+//  DataNode.hpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef DataNode_hpp
+#define DataNode_hpp
+
+#include <stdio.h>
+
+#endif /* DataNode_hpp */

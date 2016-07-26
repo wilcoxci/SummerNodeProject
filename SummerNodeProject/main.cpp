55e35ca99d06b3a3b1287477ae14532e51cdcc1f
wilcoxci <cody.henrichsen@740s-pl-998.local>
2016-07-26 14:22:38 -0600

    Initial Commit

---
+++ SummerNodeProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/26/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

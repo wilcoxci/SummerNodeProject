2fd98666ddba55e334aebc5fe4b504a89f2d9e05
wilcoxci <cody.henrichsen@740s-pl-998.local>
2016-07-27 14:05:10 -0600

    Making a list Part II

--- SummerNodeProject/Model/SummerList.cpp
+++ SummerNodeProject/Model/SummerList.cpp
@@ -7,3 +7,36 @@
 //
 
 #include "SummerList.hpp"
+#include <assert.h>
+
+template <class Type>
+SummerList<Type> :: SummerList()
+{
+    size = 0;
+    front = nullptr;
+    end = nullptr;
+}
+
+template <class Type>
+void SummerList<Type> ::   addAtIndex(int index, Type data)
+{
+    assert(index >= 0 && index <= size);
+    //Different than array -- we can dd to the end.
+    DataNode<Type> * indexPointer = front;
+    
+    
+    for(int position = 0; position < index - 1; position++)
+    {
+        indexPointer = indexPointer->getNodePointer();
+    }
+   
+    
+    DataNode<Type> * newNode = new DataNode<Type>(data);
+    
+    newNode->setNodePointer(indexPointer->getNodePointer());
+    
+    //These two lines are the same things as previous line.
+    //dataNode<Type> * temp = indexPointer->getNodePointer();
+    //newNode->setNodePointer(temp);
+    
+}

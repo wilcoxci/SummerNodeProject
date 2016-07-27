ff30ca60b15a0a4b2c2d713f74717b4a430f3054
wilcoxci <cody.henrichsen@740s-pl-998.local>
2016-07-27 09:49:13 -0600

    Summary array hpp

--- SummerNodeProject/Model/SummerArray.hpp
+++ SummerNodeProject/Model/SummerArray.hpp
@@ -10,5 +10,27 @@
 #define SummerArray_hpp
 
 #include <stdio.h>
+#include "DataNode.hpp"
+
+template <class Type>
+class SummerArray
+{
+private:
+    int length;
+    bool * modifiedNodes;
+    DataNode<Type> front;
+    DataNode<Type> end;
+public:
+    SummerArray(int length);
+    
+    Type getFromIndex(int index);
+    Type getFrist();
+    Type getLast();
+    void setAtIndex(int index, Type data);
+    
+    void sort();
+    void resize(int newSize);
+    int getUnusedNodes();
+};
 
 #endif /* SummerArray_hpp */

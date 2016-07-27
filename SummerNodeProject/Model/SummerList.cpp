3168519b002d6753c9aaca1ef9305e76f0fc79bd
wilcoxci <cody.henrichsen@740s-pl-998.local>
2016-07-27 14:57:17 -0600

    more bullshit

--- SummerNodeProject/Model/SummerList.cpp
+++ SummerNodeProject/Model/SummerList.cpp
@@ -29,13 +29,11 @@ void SummerList<Type> ::   addAtIndex(int index, Type data)
     
     if(index == 0)
     {
-        newNode->setNodePointer(indexPointer);
-        front = newNode;
+        addAtFront(data);
     }
     else if(index == size-1)
     {
-        end->setNodePointer(newNode);
-        end = newNode;
+        add(data);
     }
     else
     {
@@ -53,4 +51,37 @@ void SummerList<Type> ::   addAtIndex(int index, Type data)
     indexPointer->setNodePointer(newNode);
     }
     
+}
+template <class Type>
+void SummerList<Type> :: add(Type data)
+{
+    DataNode<Type> * newNode = new DataNode<Type>(data);
+    if(size == 0)
+    {
+        front = newNode;
+        end = newNode;
+    }
+    else
+    {
+        end->setNodePointer(newNode);
+        end = newNode;
+    }
+}
+
+template <class Type>
+void SummerList<Type> :: addAtFront(Type data)
+{
+    //Create a node.
+    DataNode<Type> * newNode = new DataNode<Type>(data);
+    if(size == 0)
+    {
+        front = newNode;
+        end = newNode;
+    }
+    else
+    {
+        newNode->setNodePointer(front);
+        front = newNode;
+    }
+
 }

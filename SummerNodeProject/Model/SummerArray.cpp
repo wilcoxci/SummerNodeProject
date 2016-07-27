e4f4cad5a7f4c425dba68acffa14e3e76532823f
wilcoxci <cody.henrichsen@740s-pl-998.local>
2016-07-27 09:18:39 -0600

    Commit Day 1 #1

---
+++ SummerNodeProject/Model/SummerArray.cpp
@@ -0,0 +1,9 @@
+//
+//  SummerArray.cpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/27/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#include "SummerArray.hpp"
---
+++ SummerNodeProject/Model/SummerArray.hpp
@@ -0,0 +1,14 @@
+//
+//  SummerArray.hpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/27/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef SummerArray_hpp
+#define SummerArray_hpp
+
+#include <stdio.h>
+
+#endif /* SummerArray_hpp */

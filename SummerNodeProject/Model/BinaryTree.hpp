4d8c460bb805a08650ca7530e8c6e22eb121c2b5
wilcoxci <cody.henrichsen@740s-pl-998.local>
2016-07-28 10:29:52 -0600

    Binary Tree Startup

---
+++ SummerNodeProject/Model/BinaryTree.hpp
@@ -0,0 +1,29 @@
+//
+//  BinaryTree.hpp
+//  SummerNodeProject
+//
+//  Created by Henrichsen, Cody on 7/28/16.
+//  Copyright © 2016 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTree_hpp
+#define BinaryTree_hpp
+
+#include <stdio.h>
+#include "BinaryTreeNode.hpp"
+
+template <class Type>
+class BinaryTree
+{
+private:
+    BinaryTreeNode<Type> * root;
+public:
+    BinaryTree();
+    void insert(Type data);
+    void inOrderTraversal();
+    void preOrderTraversal();
+    void postOrderTraversal();
+    int calculateSize();
+    bool remoe(Type data);
+};
+#endif /* BinaryTree_hpp */

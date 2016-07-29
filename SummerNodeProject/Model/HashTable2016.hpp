0a1540e608a9c4a32f0e65f45acf5f9485db76f3
wilcoxci <cody.henrichsen@740s-pl-998.local>
2016-07-29 13:00:55 -0600

    Updated the fix

--- SummerNodeProject/Model/HashTable2016.hpp
+++ SummerNodeProject/Model/HashTable2016.hpp
@@ -24,7 +24,7 @@ private:
     void resize();
     long nextPrime(long current);
     long findPosition(Type data);
-    long handleCollision(Type data);
+    long handleCollision(Type data, long currentPosition);
 public:
     HashTable();
     void add(Type data);

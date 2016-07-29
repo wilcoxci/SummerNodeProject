a4e0047a8031ba80dd14512b0ff6ffe9e158cc6d
wilcoxci <cody.henrichsen@740s-pl-998.local>
2016-07-29 10:55:08 -0600

    Just another update

--- SummerNodeProject/Model/HashTable2016.cpp
+++ SummerNodeProject/Model/HashTable2016.cpp
@@ -7,3 +7,24 @@
 //
 
 #include "HashTable2016.hpp"
+
+template <class Type>
+HashTable<Type> :: HashTable()
+{
+    this->capacity - 101;
+    this->size = 0;
+    this->efficiencyPercentage = .666;
+    
+    this->front = new HashNode<Type>();
+    HashNode<Type> * currentEnd = front;
+    //Loop to create the first array of nodes for storage.
+    for (int index = 1; index < capacity; index++)
+    {
+        HashNode<Type> * next = new HashNode<Type>();
+        currentEnd->setNode(next);
+        currentEnd = next;
+    }
+        
+}
+
+
